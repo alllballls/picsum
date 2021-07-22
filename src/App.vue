d5012c52e97760242307869c0c20e106d056e2be
alllballls <jeffmlevy@gmail.com>
2021-07-22 11:21:31 -0800

    muckin with styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+    <div>
+      <b-card-group columns>
+        <b-card
+          v-for="(p, i) in items"
+          :key="i"
+          :title="p.title"
+          :img-src="p.photo"
+          class="card-custom"
+          body-class="card-custom-body"
+          img-alt="Image of Something"
+          img-top
+        >
+          <b-card-text>
+            {{ p.text }}
+          </b-card-text>
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
+
+<script>
+import { v4 as uuidv4 } from "uuid";
+import { LoremIpsum } from "lorem-ipsum";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      items: [],
+      limit: 25,
+      lorem: new LoremIpsum({
+        sentencesPerParagraph: {
+          max: 8,
+          min: 4,
+        },
+        wordsPerSentence: {
+          max: 16,
+          min: 4,
+        },
+      }),
+    };
+  },
+  props: [],
+  components: {},
+  computed: {},
+  methods: {
+    getPhotos() {
+      for (var i = 0; i < this.limit; i++) {
+        var seed = uuidv4();
+        this.items.push({
+          title: this.lorem.generateWords(2),
+          text: this.lorem.generateSentences(1),
+          photo: `https://picsum.photos/seed/${seed}/200`,
+        });
+      }
+    },
+  },
+  created() {
+    this.getPhotos();
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  padding: 2rem;
+}
+
+.card-custom {
+  max-height: 500px;
+}
+
+.card-custom-body {
+  font-size: x-small;
+}
+</style>
+
